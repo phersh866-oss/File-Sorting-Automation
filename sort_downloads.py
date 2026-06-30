aaa1edb68b47a3c5cc48f47f5baa64a639ca0c79
phersh866-oss <phersh866@gmail.com>
2026-06-30 17:31:21 -0400

    Version 1 of Project
    Creating Path Variables

---
+++ sort_downloads.py
@@ -0,0 +1,24 @@
+# Let's Sort Your Messy Downloads Folder!
+
+from pathlib import Path
+
+# 📦 Global Variables
+#---------------------------------------------------------------------------
+PATH_DOWNLOADS = r'C:\Users\hpat2\Downloads'        # Dev/Local
+PATH_DOWNLOADS = Path.home() / 'Downloads'          # Universal
+
+# Define Sorting Rules
+
+
+# Read All Files
+
+# Get File Category (based on rules)
+
+# Create Sorting Folders
+
+# Move Files
+
+# Report Results
+
+# Bonus: Create CMD-Command
+
