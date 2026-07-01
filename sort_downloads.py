6ffcc7d09829ad003140523a26dcf672b55a9f42
phersh866-oss <phersh866@gmail.com>
2026-07-01 13:42:22 -0400

    Version 2 of Project
    I am defining the sorting rules that are needed

--- sort_downloads.py
+++ sort_downloads.py
@@ -7,7 +7,23 @@ from pathlib import Path
 PATH_DOWNLOADS = r'C:\Users\hpat2\Downloads'        # Dev/Local
 PATH_DOWNLOADS = Path.home() / 'Downloads'          # Universal
 
+
+#---------------------------------------------------------------------------
 # Define Sorting Rules
+
+# the keys are the folder names
+# the values are the type of file extension
+
+CATEGORIES = {
+    '_Images'       : ['.jpg', '.jpeg', '.png'],
+    '_GIF'          : ['.gif'],
+    '_Videos'       : ['.mp4', '.mkv', '.avi', '.mov'],
+    '_Docs'         : ['.pdf', '.docx', '.txt'],
+    '_ZIPs'         : ['.zip', '.rar', '.7z'],
+    '_Installers'   : ['.exe', '.msi'],
+    '_Code'         : ['.py', '.js', '.html', '.css', '.md'],
+    '_Data'         : ['.csv', '.xlsx']
+}
 
 
 # Read All Files
